52c6311bc09ebbc25df7fd889304a95c9425d486
Arvindh S <arvindhan_s@msn.com>
2016-09-28 14:30:39 +1300

    New Project Setup - Arvin

---
+++ EPMOMISSolution/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EPMOMISSolution.Startup))]
+namespace EPMOMISSolution
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
